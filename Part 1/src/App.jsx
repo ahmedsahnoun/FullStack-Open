39c5a54ea8fb17ac99ee07d79c16e5135d406ce1
Ahmed <sahmed3598@gmail.com>
2024-04-19 21:43:04 +0200

    ex 1.11

--- Part 1/src/App.jsx
+++ Part 1/src/App.jsx
@@ -1,48 +1,78 @@
+import { useState } from 'react'
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td> {text} </td>
+      <td> {value} {text === "positive" ? "%" : ""} </td>
+    </tr>
+  )
+}
+
+const Statistics = ({ values, all }) => {
+  if (all === 0)
+    return (
+      <div>No feedback given</div>
+    )
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text={"good"} value={values.good}></StatisticLine>
+        <StatisticLine text={"neutral"} value={values.neutral}></StatisticLine>
+        <StatisticLine text={"bad"} value={values.bad}></StatisticLine>
+        <StatisticLine text={"all"} value={values.all}></StatisticLine>
+        <StatisticLine text={"average"} value={values.average}></StatisticLine>
+        <StatisticLine text={"positive"} value={values.positive}></StatisticLine>
+      </tbody>
+    </table>
+  )
+}
+
+const Title = ({ text }) => {
+  return (
+    <h1>{text}</h1>
+  )
+}
+
+const Button = ({ text, valueSetter }) => {
+  return (
+    <button onClick={valueSetter}>{text}</button>
+  )
+}
+
 const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
-  const Header = (props) => {
-    return (
-      <h1>{props.course}</h1>
-    )
+  const updater = (type) => {
+    const newValues = { good, neutral, bad, all }
+    const valueSetters = {
+      good: setGood,
+      neutral: setNeutral,
+      bad: setBad,
+    }
+    newValues[type]++
+    newValues.all++
+
+    valueSetters[type](newValues[type])
+    setAll(newValues.all)
+    setAverage((newValues.good - newValues.bad) / newValues.all)
+    setPositive((newValues.good / newValues.all) * 100)
   }
-
-  const Content = () => {
-    return (
-      <div>
-        <Part part={part1} exercises={exercises1} />
-        <Part part={part2} exercises={exercises2} />
-        <Part part={part3} exercises={exercises3} />
-      </div>
-    )
-  }
-
-  const Total = (props) => {
-    return (
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    )
-  }
-
-  const Part = (props) => {
-    return (
-      <p>
-        {props.part} {props.exercises}
-      </p>)
-  }
-
-
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
 
   return (
     <div>
-      <Header course={course} />
-      <Content />
-      <Total />
+      <Title text={"give feedback"}></Title>
+      <Button text={"good"} valueSetter={() => updater("good")}></Button>
+      <Button text={"neutral"} valueSetter={() => updater("neutral")}></Button>
+      <Button text={"bad"} valueSetter={() => updater("bad")}></Button>
+      <Title text={"statistics"}></Title>
+      <Statistics all={all} values={{ good, bad, neutral, all, average, positive }}></Statistics>
     </div>
   )
 }
